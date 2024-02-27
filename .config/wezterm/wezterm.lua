933e440a0bb18f40b2abdd9b95f78545b8521dfd
Veit Lehmann <levito82@gmail.com>
2024-02-27 02:54:51 +0100

    add wezterm and bat config with vscode theme, extend micro theme

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,106 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+local config = wezterm.config_builder()
+
+config.colors = {
+  -- VSCode Dark+ inspired colors
+  foreground = "#ccc",
+  background = "#111",
+  cursor_fg = "#111",
+  cursor_bg = "#fff",
+  cursor_border = "#ccc",
+  selection_fg = "#fff",
+  selection_bg = "#357",
+  ansi    = { '#111', '#d66', '#695', '#c97', '#59d', '#b8b', '#4ba', '#ccc' },
+  brights = { '#444', '#f99', '#ad8', '#ed9', '#9df', '#ead', '#8dc', '#fff' },
+
+  -- Hide tab edges, tab bar background is also #333
+  tab_bar = {
+    inactive_tab_edge = "#333",
+  }
+}
+
+config.inactive_pane_hsb = {
+  brightness = 0.5,
+}
+
+if string.find(wezterm.target_triple, 'darwin') then
+  -- Bindings for Mac
+  config.keys = {
+    {
+      key = 'D',
+      mods = 'SUPER|SHIFT',
+      action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+    },
+    {
+      key = 'D',
+      mods = 'SUPER|SHIFT|ALT',
+      action = act.SplitVertical({ domain = 'CurrentPaneDomain' }),
+    },
+    {
+      key = 'Enter',
+      mods = 'SUPER|SHIFT',
+      action = act.TogglePaneZoomState,
+    },
+    {
+      key = 'LeftArrow',
+      mods = 'SUPER',
+      action = act.ActivatePaneDirection('Left'),
+    },
+    {
+      key = 'RightArrow',
+      mods = 'SUPER',
+      action = act.ActivatePaneDirection('Right'),
+    },
+    {
+      key = 'UpArrow',
+      mods = 'SUPER',
+      action = act.ActivatePaneDirection('Up'),
+    },
+    {
+      key = 'DownArrow',
+      mods = 'SUPER',
+      action = act.ActivatePaneDirection('Down'),
+    },
+    {
+      key = 'LeftArrow',
+      mods = 'SUPER|ALT',
+      action = act.ActivateTabRelative(-1),
+    },
+    {
+      key = 'RightArrow',
+      mods = 'SUPER|ALT',
+      action = act.ActivateTabRelative(1),
+    },
+  }
+else
+  -- Bindings for Linux
+  config.keys = {
+    {
+      key = 'D',
+      mods = 'CTRL|SHIFT',
+      action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+    },
+    {
+      key = 'D',
+      mods = 'CTRL|SHIFT|ALT',
+      action = act.SplitVertical({ domain = 'CurrentPaneDomain' }),
+    },
+    {
+      key = 'Enter',
+      mods = 'CTRL|SHIFT',
+      action = act.TogglePaneZoomState,
+    },
+  }
+
+  -- Font tuning for Linux
+  config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
+  config.font_size = 9
+  config.freetype_load_target = 'HorizontalLcd'
+
+  config.window_frame = {
+    font_size = 9,
+  }
+end
+
+return config
--- .github/README.md
+++ .github/README.md
@@ -17,7 +17,7 @@ fzf git git-delta kdiff3 micro
 
 Optionally install additional software:
 ```
-android-sdk android-studio aspell broot bun fd gh git-gui ripgrep tig yazi
+android-sdk android-studio aspell bat broot bun fd gh git-gui ripgrep tig wezterm yazi
 ```
 
 Install plugins for the micro editor:
@@ -27,7 +27,7 @@ aspell editorconfig filemanager fzf joinLines quoter wc
 
 Some more recommended software (not configured/tracked via dotfiles yet, or no config needed):
 ```
-bat encfs eza gocryptfs jq mdcat sirikali textadept trash visual-studio-code volta wezterm
+encfs eza gocryptfs jq mdcat sirikali textadept trash visual-studio-code volta
 ```
 
 Recommended global npm modules (install via `volta install`):
